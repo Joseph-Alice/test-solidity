2c04794511d2ff05057f41c9729f9bab81fe114c
pengxb <master@pengxb.com>
2023-10-20 15:10:23 +0800

    Dtop

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,13 +2,13 @@
  * @Author: pengxb master@pengxb.com
  * @Date: 2023-10-16 10:00:17
  * @LastEditors: pengxb master@pengxb.com
- * @LastEditTime: 2023-10-17 19:21:03
- * @FilePath: /truffle/migrations/deploy_contracts.js
+ * @LastEditTime: 2023-10-20 15:03:36
+ * @FilePath: /test-solidity/migrations/2_deploy_contracts.js
  * @Description: 迁移文件来部署合约
  * Copyright (c) 2023 by pengxb email: master@pengxb.com, All Rights Reserved.
  */
-const MyToken = artifacts.require("MyToken");
+const Dtop = artifacts.require("Dtop");
 
 module.exports = function (deployer) {
-  deployer.deploy(MyToken, 1000000); // 初始供应量为 1,000,000 tokens
+  deployer.deploy(Dtop); // 初始供应量为 1,000,000 tokens
 };
