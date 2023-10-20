352ae636cfcc7a10a64d00667e31d338f33979f8
pengxb <master@pengxb.com>
2023-10-20 14:31:01 +0800

    Dtop

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,13 +2,13 @@
  * @Author: pengxb master@pengxb.com
  * @Date: 2023-10-16 10:00:17
  * @LastEditors: pengxb master@pengxb.com
- * @LastEditTime: 2023-10-20 13:45:44
+ * @LastEditTime: 2023-10-20 14:26:23
  * @FilePath: /test-solidity/migrations/2_deploy_contracts.js
  * @Description: 迁移文件来部署合约
  * Copyright (c) 2023 by pengxb email: master@pengxb.com, All Rights Reserved.
  */
-const MyToken = artifacts.require("MyToken");
+const Dtop = artifacts.require("Dtop");
 
 module.exports = function (deployer) {
-  deployer.deploy(MyToken);
+  deployer.deploy(Dtop);
 };
